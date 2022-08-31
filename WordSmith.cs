f2f5ea1c4228db5fecbeb3c70d580e1aca7f598f
Derek Griffin <griffinderek6@gmail.com>
2022-08-31 08:46:53 -0600

    finished

---
+++ WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //reverse the word
+            var reversed = "";
+            for (int i = word.Length - 1; i >= word.Length; i--)
+            {
+                reversed += word[i]; 
+            }
+            //compare the original with the reversed
+            if(reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //return wether they are the same or not
+        }
+    }
+}
+
